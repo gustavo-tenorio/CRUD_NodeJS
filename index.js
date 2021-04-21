b8d99db25a127020536bbb51a0878d0e3f7baf94
gustavo <kingxsite@hotmail.com>
2021-04-20 22:26:22 -0300

    Tela de cadastro

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
-    res.send('Hello World!');
+    res.render('index');
 });
 
 
